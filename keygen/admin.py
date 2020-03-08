f4735cc7162979ea6f4cc6916dea6d5fc3df0073
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-08 17:52:34 +0530

    Added admin for key

--- keygen/admin.py
+++ keygen/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import register
+
+from keygen.models import Key
+
+
+@register(Key)
+class KeyAdmin(admin.ModelAdmin):
+    list_display = ('val', 'is_used',)
