92f1546b8624f6cc5ed13f1f2f58e93ccf6166c8
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-09 05:14:52 +0530

    Added redis to settings

--- potato_url/settings.py
+++ potato_url/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import redis as redis
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -101,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REDIS_POOL = redis.ConnectionPool(host='localhost', port=6379, db=0)
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
