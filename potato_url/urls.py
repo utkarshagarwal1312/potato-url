626283fb2a8dc2394913f635e0c91e0be4fec0fa
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-08 17:44:49 +0530

    Changed project from _32 to potato_url

--- potato_url/urls.py
+++ potato_url/urls.py
@@ -1,4 +1,4 @@
-"""_32 URL Configuration
+"""potato_url URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
