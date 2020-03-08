0ecbfb0f553c9ba6c3973ce20a53c9ad4150afb8
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-08 18:29:29 +0530

    Added urls and views and serializer base for core

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
 
 # Create your views here.
+from rest_framework.response import Response
+
+from core.serializers import RedirectionSerializer
+
+
+class Redirection(GenericAPIView):
+    serializer_class = RedirectionSerializer
+
+    def get(self, request):
+        return Response(status=301)
+
