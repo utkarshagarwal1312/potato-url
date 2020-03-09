dbe5ebd542eb4c4323b055e5692112d33451d6d0
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-09 07:00:53 +0530

    Added todo for model

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from keygen.models import Key
 
 
 class KeyUrlMap(models.Model):
+    # TODO : Add Integrity Error in save in case two active maps for same key.
     key = models.ForeignKey(Key, on_delete=models.CASCADE)
     url = models.TextField()
     is_active = models.BooleanField(default=True)
