5f555cea990c03d3f5031c17359535f9b01036b0
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-09 06:59:44 +0530

    Added model and migration for KeyUrlMap

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+from keygen.models import Key
+
+
+class KeyUrlMap(models.Model):
+    key = models.ForeignKey(Key, on_delete=models.CASCADE)
+    url = models.TextField()
+    is_active = models.BooleanField(default=True)
