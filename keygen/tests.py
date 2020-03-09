ea45ef0db8322935c885e55c9906a200df1f9e82
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-09 05:45:15 +0530

    Added test for keypolgenerator

--- keygen/tests.py
+++ keygen/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+from keygen.models import Key
+from keygen.utils.key_pool import KeyPoolGenerator
+
+
 # Create your tests here.
+class UtilsTest(TestCase):
+    def test_util_key_pool_generator(self):
+        KeyPoolGenerator(number_of_keys=100).generate_keys()
+        self.assertEqual(Key.objects.all().count(), 100)
