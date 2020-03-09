b6ba9043cff835417db0ca8c22c731709b0b4867
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-09 06:55:40 +0530

    Added todo

--- keygen/tests.py
+++ keygen/tests.py
@@ -6,6 +6,7 @@ from keygen.utils.key_pool import KeyPoolGenerator
 
 # Create your tests here.
 class UtilsTest(TestCase):
+    # TODO : Add redis for test, or use different set for test
     def test_util_key_pool_generator(self):
         KeyPoolGenerator(number_of_keys=100).generate_keys()
         self.assertEqual(Key.objects.filter(is_used=False).count(), 80)
