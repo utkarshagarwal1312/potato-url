ddfc47a3601001bc1c5dca678adf58dd47eb7fd0
utkarshagarwal1312 <utkarsh.agarwal@codenation.co.in>
2020-03-09 05:38:57 +0530

    Removed bulk create and added logging with change in logic

--- keygen/utils/key_pool.py
+++ keygen/utils/key_pool.py
@@ -1,6 +1,7 @@
 import string
 import logging
-from random import random
+
+import random
 import redis
 from keygen.models import Key
 from potato_url.settings import REDIS_POOL
@@ -11,12 +12,19 @@ logger = logging.getLogger(__name__)
 
 
 class KeyPoolGenerator:
+    # TODO :
+    #  1. Find a better way to generate keys such that Key obj(id=k) and Key obj(id=k+1) have no
+    #  direct relationship.
+    #  2. Add bulk create for keys
+
     def __init__(self, number_of_keys=1000):
         self.number_of_keys = number_of_keys
+        self.same_key_generated = False
 
     @staticmethod
     def _random_string_generator(str_size=6, allowed_chars=string.ascii_letters + '0123456789'):
-        return ''.join(random.choice(allowed_chars) for x in range(str_size))
+        key = ''.join(random.choice(allowed_chars) for x in range(str_size))
+        return key
 
     @staticmethod
     def _check_key_already_exists(key):
@@ -29,24 +37,21 @@ class KeyPoolGenerator:
             if count > 1000:
                 return None
             elif self._check_key_already_exists(key):
+                self.same_key_generated = True
                 count += 1
                 key = self._random_string_generator()
             else:
+                Key.objects.create(val=key)
                 return key
 
-    def _generate_keys(self):
-        generated_keys = []
+    def generate_keys(self):
+        generation_error = True
         for i in range(self.number_of_keys):
             unique_key = self._generate_unique_key()
             if not unique_key:
                 break
-            generated_keys.append(Key(val=unique_key))
-        if generated_keys:
-            Key.objects.bulk_create(generated_keys, batch_size=len(generated_keys), ignore_conflicts=True)
-            return len(generated_keys)
-        return 0
+        else:
+            generation_error = False
 
-    def generate(self):
-        number_of_keys_generated = self._generate_keys()
-        if number_of_keys_generated < self.number_of_keys:
+        if generation_error:
             logger.exception("Key Generation could not create required keys.")
